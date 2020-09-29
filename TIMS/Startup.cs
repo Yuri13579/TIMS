e74043976adf9d2495f18334c91b5ef4e2d0a984
Yuri13579 <33569068+Yuri13579@users.noreply.github.com>
2020-09-29 22:42:13 +0300

    add Domain

--- TIMS/Startup.cs
+++ TIMS/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -61,6 +63,7 @@ namespace TIMS
             });
 
             services.AddControllers();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
